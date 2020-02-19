ae00616a89c3b29e1729cc3840723e051449f301
Wisarut Tiyamanee <jtiyamanee@hotmail.com>
2020-02-19 21:51:08 +0700

    Flexible content on home page

--- src/components/FirstLanding.js
+++ src/components/FirstLanding.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import {Paper,Grid,Button} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import firstPic from '../pic/firstPic.png';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center"
+    },
+    margin: {
+        margin: theme.spacing(1),
+        width: "170px"
+    }
+}));
+
 const FirstLanding = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            <Paper square elevation={0} fullWidth style={{marginTop:"70px",minHeight:'520px'}}>
-                <Grid direction='row' container>
-                    <Grid container item sm={12} md={6} xl={6} style={{marginTop:'20px'}}>
-                        <img id='picFirstLanding' src={firstPic} width='600' height='400' style={{ marginLeft: "20%" }}/>
-                    </Grid>
-                    <Grid item sm={12} md={6} xl={6} style={{paddingRight:'10px'}}>
-                        <div style={{marginTop:'10%'}}>
-                            <h1>WELCOME TO CU PART-TIME!</h1>
-                            <h2>Discovering your talent in any career fields as you desire.</h2>
-                            <p>CU PART-TIME is a platform to help you find jobs or activities based on your interests. Developed by a group of students that understand how hard it is to find jobs in real environment. 
-                                By using this platfrom, you will be working with people who have the same passion in a friendly environment. We are here to improve your experience and skill in career fields. Thank you for choosing CU PART-TIME!
-                            </p>
-                        </div>
-                    </Grid>
-                
+        <div className={classes.root} style={{ padding: '40px', minHeight:'520px' }}>
+            <Grid container spacing={3}>
+                <Grid item xs={12} sm={5}>
+                    <img 
+                        id='picFirstLanding' 
+                        src={firstPic} 
+                        style={{ width: '100%', marginLeft: '10%', marginTop: '10%'}}/>
                 </Grid>
-            </Paper>
+                <Grid item xs={12} sm={7}>
+                    <div className={classes.root} style={{ paddingTop: '10%' }}>
+                        <h1>WELCOME TO CU PART-TIME!</h1>
+                        <h2>Discovering your talent in any career fields as you desire.</h2>
+                        <p>CU PART-TIME is a platform to help you find jobs or activities based on your interests. Developed by a group of students that understand how hard it is to find jobs in real environment. 
+                        By using this platfrom, you will be working with people who have the same passion in a friendly environment. We are here to improve your experience and skill in career fields. Thank you for choosing CU PART-TIME!
+                        </p>
+                        <Button variant="contained" size="medium" color="secondary" className={classes.margin} href="/register">
+                            Get Started
+                        </Button>
+                    </div>
+                </Grid>
+            </Grid>
           
         </div>
     );
