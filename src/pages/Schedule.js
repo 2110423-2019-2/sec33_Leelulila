5c5be060c856310eeeb3372d5dd44b3a65a0815a
Nai131 <Natthapath47005@gmail.com>
2020-03-11 11:07:48 +0700

    Calendar v.3

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -9,64 +9,64 @@ import MyCalendar from '../components/myCalendar';
 
 class Profile extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            user : [],
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: [],
     }
+  }
 
-    componentDidMount() {
-        this.authListener();
-        this.getProfile();
-    }
+  componentDidMount() {
+    this.authListener();
+    this.getProfile();
+  }
 
-    getProfile() {
-        var user = fire.auth().currentUser;
-        let self = this;
-        fetch("/useremail/" + user.email, {
-          method: 'GET',
-          headers: { 'Content-Type': 'application/json' },
-        }).then(function (response) {
-          if (response.status >= 400) {
-            throw new Error("Bad response from server");
-          }
-          return response.json();
-        }).then(function (jsonData) {
-          self.setState({ user: jsonData });
-          return this.state.listing;
-        }).catch(function (err) {
-          console.log(err);
-        });
+  getProfile() {
+    var user = fire.auth().currentUser;
+    let self = this;
+    fetch("/useremail/" + user.email, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    }).then(function (response) {
+      if (response.status >= 400) {
+        throw new Error("Bad response from server");
       }
+      return response.json();
+    }).then(function (jsonData) {
+      self.setState({ user: jsonData });
+      return this.state.listing;
+    }).catch(function (err) {
+      console.log(err);
+    });
+  }
 
-    authListener() {
-        fire.auth().onAuthStateChanged((user) => {
-            if (user) {
-                this.setState({ user });
-            } else {
-                this.setState({ user: null });
-            }
-        })
-    }
+  authListener() {
+    fire.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user });
+      } else {
+        this.setState({ user: null });
+      }
+    })
+  }
 
-    render() {
-        if (this.state.user.currentJob === undefined && this.state.user.pendingJob === undefined) return null
-        else
-        console.log(this.state.user.currentJob+' propstransfer')
-        return (
-            <div > 
-                <Grid style={{ display: 'flex' }}>
-                    <Grid item sm={8} style={{ marginLeft: '5px', marginRight: '5px' , marginTop: '100px'}}>
-                    <MyCalendar/>   
-                    </Grid>
-                    <Grid item sm={4} style={{ marginLeft: '5px', marginRight: '5px', marginTop: '5px' }}>
-                    <JobListCard currentJob = {this.state.user.currentJob} pendingJob = {this.state.user.pendingJob} id = {'idkrub'}/>
-                    </Grid>                    
-                </Grid>
-            </div>);
-        
-    }
+  render() {
+    if (this.state.user.currentJob === undefined && this.state.user.pendingJob === undefined) return null
+    else
+      console.log(this.state.user.currentJob + ' propstransfer')
+    return (
+      <div >
+        <Grid style={{ display: 'flex' }}>
+          <Grid item sm={8} style={{ marginLeft: '5px', marginRight: '5px', marginTop: '100px', display: 'flex', justifyContent: 'center' }}>
+            <MyCalendar currentJob={this.state.user.currentJob} />
+          </Grid>
+          <Grid item sm={4} style={{ marginLeft: '5px', marginRight: '5px', marginTop: '5px' }}>
+            <JobListCard currentJob={this.state.user.currentJob} pendingJob={this.state.user.pendingJob} id={'idkrub'} />
+          </Grid>
+        </Grid>
+      </div>);
+
+  }
 }
 
 export default Profile;
