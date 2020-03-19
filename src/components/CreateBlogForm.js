05b6ebfbbd28fe4aa22ac102d3763744df07ca6a
Nai131 <Natthapath47005@gmail.com>
2020-03-19 19:49:57 +0700

    url check

--- src/components/CreateBlogForm.js
+++ src/components/CreateBlogForm.js
@@ -3,7 +3,6 @@ import TextField from '@material-ui/core/TextField';
 import { InputLabel, InputBase, Button, Grid } from '@material-ui/core';
 // import fire from '../config/Fire';
 import { Redirect } from 'react-router-dom';
-import DatePicker from '../components/DatePicker';
 import fire from '../config/firebase';
 
 class CreateBlogForm extends Component {
@@ -17,30 +16,38 @@ class CreateBlogForm extends Component {
         }
     }
 
-    onCreateBlog() {
-        console.log("Create Blog success");
-    }
-
-
+    validURL(str) {
+        var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+          '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+          '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+          '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+          '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+          '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+        return pattern.test(str);
+      }
 
     //push data to mongoDB
-    onCreatejob() {
+    onCreateBlog() {
         let timer = null;
         //get all data from element below
         var data = {
             BlogName: document.getElementById('blogname').value,
             BlogDetail: document.getElementById('detail').value,
             BlogTopic: document.getElementById('topic').value,
+            BlogImage: document.getElementById('image').value,
             Employer: fire.auth().currentUser.email,
             Status: "Ready"
         }
-        if (data.BlogName.length == 0 || data.BlogDetail.length == 0 || data.BlogTopic.length ==0) {
+        if (data.BlogName.length == 0 || data.BlogDetail.length == 0 || data.BlogTopic.length == 0 || data.BlogImage.length == 0) {
             alert("Please fill the Empty Box")
+        }
+        else if(!this.validURL(document.getElementById('image').value)){
+            alert("Please fill the valid URL")
         }
         else {
             alert("Your blog is being added!")
             //this function will push data to db
-            this.mongoCreateJob(data);
+            this.mongoCreateBlog(data);
             timer = setTimeout(() => this.setState({ redirect: true }), 500)
 
         }
@@ -49,8 +56,8 @@ class CreateBlogForm extends Component {
 
 
 
-    mongoCreateJob(data) {
-        
+    mongoCreateBlog(data) {
+
         fetch("/newblog", {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -72,6 +79,9 @@ class CreateBlogForm extends Component {
 
     render() {
         const { redirect } = this.state;
+        if (redirect) {
+            return <Redirect to='/Blog' />;
+        }
 
         return (
 
@@ -83,14 +93,18 @@ class CreateBlogForm extends Component {
                             <h3> Title : </h3>
                             <TextField inputProps={{ maxLength: 30 }} name='Blogname' id="blogname" color="primary" variant="outlined" margin='dense' style={{ marginLeft: '20px', width: '300px' }} />
                             <h3 style={{ "padding-left": "20px" }}>Topic :</h3>
-                            <TextField inputProps={{ maxLength: 15 }} name='Topic' color="primary" id='topic' variant="outlined" margin='dense' style={{ marginLeft: '16px', width: '178px' }} />
+                            <TextField inputProps={{ maxLength: 20 }} name='Topic' color="primary" id='topic' variant="outlined" margin='dense' style={{ marginLeft: '16px', width: '178px' }} />
+                        </Grid>
+                        <Grid style={{ margin: '16px' }}>
+                            <h3> Image (URL) : </h3>
+                            <TextField name='image' id="image" color="primary" variant="outlined" margin='dense' style={{ width: '810px' }} />
                         </Grid>
                         <Grid style={{ margin: '16px' }}>
                             <h3> Body :</h3>
-                            <TextField multiline={true} rows={10} color="primary" name='detail' id="detail" variant="outlined" margin='dense' style={{ width: '794px'}} />
+                            <TextField multiline={true} rows={10} color="primary" name='detail' id="detail" variant="outlined" margin='dense' style={{ width: '810px' }} />
                         </Grid>
                         <Grid style={{ marginTop: '32px', display: 'flex', justifyContent: 'center' }}>
-                            <Button variant="contained" color='primary' style={{ backgroundColor: '#2a3649' }} onClick={this.onCreatejob} >Create Blog</Button>
+                            <Button variant="contained" color='primary' style={{ backgroundColor: '#2a3649' }} onClick={this.onCreateBlog} >Create Blog</Button>
                         </Grid>
                     </Grid>
                 </form>
