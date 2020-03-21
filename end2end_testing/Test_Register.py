f0a5484b635dd91522e7191df5946407245bfe77
drive087 <d.thus_sk135@hotmail.com>
2020-03-21 20:41:06 +0700

    add register test

--- end2end_testing/Test_Register.py
+++ end2end_testing/Test_Register.py
@@ -2,6 +2,8 @@ from Login import Login
 from selenium import webdriver
 import time
 import unittest
+import random
+
 class Test_Register(unittest.TestCase):
 
     @classmethod
@@ -13,47 +15,71 @@ class Test_Register(unittest.TestCase):
         cls.driver.implicitly_wait(10)
         cls.driver.maximize_window()
 
-    def test_resgister_black_text_field_fail(self):
+    def test_01_resgister_fail(self):
         driver = self.driver
         driver.get("http://localhost:3000/register")
         time.sleep(2)
 
-        driver.find_element_by_id('registerBtn').click()
-        time.sleep(5)
+        driver.find_elements_by_class_name('MuiButton-label')[3].click()
+        time.sleep(2)
 
-        # driver.find_element_by_id('pass').send_keys('12345')
-        # time.sleep(2)
+        try:
+            alert = driver.switch_to_alert()
+            alert.accept()
+            print ("alert accepted")
+            assert True == True
 
-        # driver.find_element_by_id('loginBtn').click()
-        # time.sleep(5)
-
-        # try:
-        #     alert = driver.switch_to_alert()
-        #     alert.accept()
-        #     print ("alert accepted")
-        #     assert True == True
-
-        # except:
-        #     print('alert fail')
-        #     assert False == True
+        except:
+            print('alert fail')
+            assert False == True
         
-    def test_register_success(self):
-        
+        driver.get("http://localhost:3000/register")
+
+    def test_02_register_success(self):
+
         driver = self.driver
         driver.get("http://localhost:3000/register")
         time.sleep(2)
 
-        driver.find_element_by_id('email').send_keys('drive@hotmail.com')
-        time.sleep(2)
 
-        driver.find_element_by_id('pass').send_keys('123456')
-        time.sleep(2)
-        print(driver.find_elements_by_class_name().text)
+        ran = int(random.random()*100000)
+
+        ran = str(ran)
+
+        email = 'e2etest'+ ran + '@hotmail.com'
+
+        driver.find_element_by_id('email').send_keys(email) #please change
+
+        driver.find_element_by_id('password').send_keys('123456')
 
         driver.find_element_by_id('confirm-password').send_keys('123456')
+
+        driver.find_element_by_id('firstName').send_keys('e2e')
+
+        driver.find_element_by_id('lastName').send_keys('test')
+
+        driver.find_element_by_id('birthday').send_keys('17052542')
+
         time.sleep(2)
 
-        driver.find_element_by_id('loginBtn').click()
+        driver.find_elements_by_class_name('MuiButton-label')[3].click()
+
+        time.sleep(4)
+
+        try:
+            alert = driver.switch_to_alert()
+            alert.accept()
+            print ("alert accepted")
+            assert True == True
+
+        except:
+            print('alert fail')
+            assert False == True
+
+        time.sleep(2)
+
+        assert 'All Jobs' == driver.find_element_by_id('dashboard-title').text
+        
         time.sleep(5)
         
 
